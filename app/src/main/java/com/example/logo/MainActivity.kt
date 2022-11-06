206f6828a4fd09c9e6d779fce4aa92cd052776af
Qugerd <qugerd.main@gmail.com>
2022-11-06 22:53:11 +1000

    Initial commit

---
+++ app/src/main/java/com/example/logo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.logo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_goods)
+    }
+}
