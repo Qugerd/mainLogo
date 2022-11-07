041e7287a82ddd4aba4151b65f21be7891eb9750
Qugerd <qugerd.main@gmail.com>
2022-11-07 21:02:49 +1000

    add classes ui

---
+++ app/src/main/java/com/example/logo/ui/profile/ProfileFragment.kt
@@ -0,0 +1,24 @@
+package com.example.logo.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.logo.databinding.FragmentProfileBinding
+
+class ProfileFragment: Fragment(){
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
